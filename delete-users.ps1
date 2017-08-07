8bf0180a9e1e1c7b0c68299956af08ead924988b
OktaSupport <30735117+OktaSupport@users.noreply.github.com>
2017-08-07 15:43:01 -0700

    Update delete-users.ps1

--- delete-users.ps1
+++ delete-users.ps1
@@ -1,4 +1,3 @@
-# Disclaimer: an Okta user deletion is a permanent process that cannot be reversed. Use these scripts at your own risk. All scripts are provided AS IS without warranty of any kind. Okta disclaims all implied warranties including, without limitation, any implied warranties of fitness for a particular purpose. We highly recommend testing scripts in a preview environment if possible.
 
 param([string]$orgurl , [string]$apikey, [string]$filepath)
 
